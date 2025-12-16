e454fb4d17e489d2e823e0c5e5b94c50dc7ee41b
Sahil Panchal <sahilpanchal1818@gmail.com>
2025-12-16 19:09:06 +0530

    Update

--- components/AuthPage.tsx
+++ components/AuthPage.tsx
@@ -203,7 +203,7 @@ export const AuthPage: React.FC<AuthPageProps> = ({ onLogin }) => {
                   value={name}
                   onChange={(e) => setName(e.target.value)}
                   className="w-full pl-10 pr-4 py-2.5 bg-slate-50 border border-slate-200 rounded-lg focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-100 transition-all"
-                  placeholder="John Doe"
+                  placeholder="Sahil Panchal"
                 />
               </div>
             </div>
